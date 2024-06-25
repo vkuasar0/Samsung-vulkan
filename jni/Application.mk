4329e55d141f77a207429d2b7df78293450f5987
Vishwa Karthik <vvishwakarthik@outlook.com>
2024-06-25 10:46:00 +0530

    Changed from Cmake to ndk-build

---
+++ jni/Application.mk
@@ -0,0 +1,7 @@
+APP_ABI := arm64-v8a
+APP_STL := c++_static
+APP_CPPFLAGS := -frtti -fexceptions -fstack-protector
+APP_PLATFORM := latest
+NDK_TOOLCHAIN_VERSION := clang
+
+
--- scripts/adb_push.sh
+++ scripts/adb_push.sh
@@ -1,5 +1,5 @@
 # adb push arm64-v8a/libvulkan.so /system/vendor/lib
-adb push src/vulkan_compute /data/local/tmp
+adb push libs/arm64-v8a/vulkan_compute /data/local/tmp
 adb push image_add1.png /data/local/tmp
 adb push image_add2.png /data/local/tmp
 adb shell /data/local/tmp/vulkan_compute
