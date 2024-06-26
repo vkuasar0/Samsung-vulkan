a9f6ec0ca5ee1a2c2292fe1967ea2a4d29696040
Vishwa Karthik <vvishwakarthik@outlook.com>
2024-06-26 19:33:01 +0530

    Crop + Emboss shaders

---
+++ scripts/adb_pull.sh
@@ -0,0 +1,2 @@
+adb pull /data/local/tmp/result.jpg output/
+adb pull /data/local/tmp/report_vulkan.csv output/
--- scripts/gaussian_push.sh
+++ scripts/gaussian_push.sh
@@ -1,5 +1,6 @@
-# adb push arm64-v8a/libvulkan.so /system/vendor/lib
+glslc -fshader-stage=compute -o shaders/compute.spv shaders/gaussian.comp
 adb push libs/arm64-v8a/vulkan_compute /data/local/tmp
 adb push image_add1.png /data/local/tmp
 adb push image_add2.png /data/local/tmp
-adb shell /data/local/tmp/vulkan_compute
+adb push shaders /data/local/tmp
+adb shell
