d4b73ae60c915a19fb912f1e67f0723d192728da
AdityaRavi7 <adityaravi04@gmail.com>
2024-10-29 00:22:12 +0530

    Final 20 shaders

---
+++ scripts/median_push.sh
@@ -0,0 +1,6 @@
+glslc -fshader-stage=compute -o shaders/compute.spv shaders/medianFilter.comp
+adb push libs/arm64-v8a/vulkan_compute /data/local/tmp
+adb push image_add1.png /data/local/tmp
+adb push image_add2.png /data/local/tmp
+adb push shaders /data/local/tmp
+adb shell
